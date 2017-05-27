e539393cc1d5a65b9eaf0e6f0e221a2f149ce1e2
unknown <wangjieyao@tclking.com>
2017-05-27 11:46:33 +0800

    commit spring-mixedconfig

---
+++ spring-mixedconfig/src/main/java/soundsystem/SoundSystemConfig.java
@@ -0,0 +1,12 @@
+package soundsystem;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ImportResource;
+
+@Configuration
+@ImportResource("classpath:cd-config.xml")
+@Import({CDPlayerConfig.class})
+public class SoundSystemConfig {
+
+}
