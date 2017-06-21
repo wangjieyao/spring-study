3364695bbeb36d1106343123cde0cdb2252e60e0
unknown <wangjieyao@tclking.com>
2017-06-21 15:20:19 +0800

    update spring-thymeleaf

--- spring-thymeleaf/src/main/java/spittr/config/SecurityConfig.java
+++ spring-thymeleaf/src/main/java/spittr/config/SecurityConfig.java
@@ -18,44 +18,44 @@ import javax.sql.DataSource;
 @EnableWebMvcSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-//  @Override
-//  protected void configure(HttpSecurity http) throws Exception {
-//    http
-//      .formLogin()
-//        .loginPage("/login")
-//      .and()
-//        .logout()
-//          .logoutSuccessUrl("/")
-//      .and()
-//      .rememberMe()
-//        .tokenRepository(new InMemoryTokenRepositoryImpl())
-//        .tokenValiditySeconds(2419200)
-//        .key("spittrKey")
-//      .and()
-//       .httpBasic()
-//         .realmName("Spittr")
-//      .and()
-//      .authorizeRequests()
-//        .antMatchers("/").authenticated()
-//        .antMatchers("/spitter/me").authenticated()
-//        .antMatchers(HttpMethod.POST, "/spittles").authenticated()
-//        .anyRequest().permitAll();
-//  }
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http
+      .formLogin()
+        .loginPage("/login")
+      .and()
+        .logout()
+          .logoutSuccessUrl("/")
+      .and()
+      .rememberMe()
+        .tokenRepository(new InMemoryTokenRepositoryImpl())
+        .tokenValiditySeconds(2419200)
+        .key("spittrKey")
+      .and()
+       .httpBasic()
+         .realmName("Spittr")
+      .and()
+      .authorizeRequests()
+        .antMatchers("/").authenticated()
+        .antMatchers("/spitter/me").authenticated()
+        .antMatchers(HttpMethod.POST, "/spittles").authenticated()
+        .anyRequest().permitAll();
+  }
 
-    /**
+   /* *//**
      * 基于内存的用户存储
      * @param auth
      * @throws Exception
-     */
-//  @Override
-//  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//    auth
-//      .inMemoryAuthentication()
-//            .withUser("user").password("password").roles("USER").and()
-//            .withUser("admin").password("password").roles("USER","ADMIN");
-//  }
-//
- /* @Autowired
+     *//*
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth
+      .inMemoryAuthentication()
+            .withUser("user").password("password").roles("USER").and()
+            .withUser("admin").password("password").roles("USER","ADMIN");
+  }*/
+
+  @Autowired
   private DataSource dataSource;
 
   @Override
@@ -65,8 +65,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .usersByUsernameQuery("select username,password,true from spitter where username=?")
             .authoritiesByUsernameQuery("select username,'ROLE_USER' from spitter where username=?");
 //    .passwordEncoder(new StandardPasswordEncoder("53cr3t"));
-  }*/
+  }
 
+/*
+    *//**
+     * 配置自定义的用户认证服务
+     *//*
     @Autowired
     SpitterRepository spitterRepository;
 
@@ -74,8 +78,31 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         //使用SpitterUserService进行用户认证
         auth.userDetailsService(new SpitterUserService(spitterRepository));
-    }
+    }*/
+
+   /* @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //指明/spiters*//** /spittles/mine  /spittles的pos方法需要进行认证，其他请求不需要
+//        http.authorizeRequests().antMatchers("/spiters*//**","/spittles/mine").authenticated()
+//                .antMatchers(HttpMethod.POST,"/spittles").hasAuthority("ROLE_USER")
+//                .anyRequest().permitAll();
+//        等价于antMatchers("/spiters*//**"）
+//        http.authorizeRequests().regexMatchers("/spitters/.*").authenticated().anyRequest().permitAll();
 
 
+//        http.authorizeRequests().antMatchers("/spitter/me").hasAuthority("ROLE_USER")
+//                .antMatchers(HttpMethod.POST,"/spittles").authenticated()
+//                .anyRequest().permitAll();
 
+        http .formLogin()//启用默认的登录页面
+        .loginPage("/login").and()
+        .logout().logoutSuccessUrl("/").and()
+        *//*.rememberMe().tokenValiditySeconds(2419200).key("spitterkey").and()//启用remember-me 有效时间为4周 设置私钥*//*
+        .authorizeRequests().antMatchers("/spitter/me")
+        .access("hasRole('ROLE_USER') and hasIpAddress('127.0.0.1')")
+        .anyRequest().permitAll()
+        *//* .and().httpBasic().realmName("Spitter") //启用http basic*//*
+        *//*.and()//设置安全通道 会将请求重定向到https
+        .requiresChannel().antMatchers("/spitter/me").requiresSecure()*//*;
+    }*/
 }
