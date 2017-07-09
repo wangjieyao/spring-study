99fc5e784cb625bf342941139d70956dbf77c80f
master <501911453@qq.com>
2017-07-09 17:04:42 +0800

    add spring-cache

---
+++ spring-cache/src/main/java/cache/db/SpitterRepository.java
@@ -0,0 +1,24 @@
+package cache.db;
+
+import cache.bean.Spitter;
+
+import java.util.List;
+
+
+/**
+ * Repository interface with operations for {@link Spitter} persistence.
+ * @author habuma
+ */
+public interface SpitterRepository {
+
+  long count();
+  
+  Spitter save(Spitter spitter);
+  
+  Spitter findOne(long id);
+
+  Spitter findByUsername(String username);
+
+  List<Spitter> findAll();
+
+}
